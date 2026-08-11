740ebd93f3cfadb1d85d7ce3cbc084e14ce26f4c
Egor Efanov <egorefanov7117@gmail.com>
2026-08-11 15:08:01 +0300

    feat: add show all to book menu

--- ui/book_menu.py
+++ ui/book_menu.py
@@ -1,3 +1,4 @@
+from models.book import Book
 
 class BookMenu:
 
@@ -33,3 +34,45 @@ class BookMenu:
                 break
             else:
                 print("\nОшибка: такого пункта меню нет.")
+
+    def show_all(self) -> None:
+        books = self.book_service.get_all()
+
+        if not books:
+            print("\nКниг нет.")
+            return
+        
+        sort = input("\nСортировать? (y/n): ").lower()
+
+        if sort == "y":
+            print("Функция сортировки еще не реализвана")
+
+        filter = input("\nФильтровать? (y/n): ").lower()
+
+        if filter == "y":
+            print("Функция фильтрации еще не реализована")
+
+        self._print_books(books)
+        
+    def _print_books(self, books: list[Book]) -> None:
+        print("\n+" + "-" * 103 + "+")
+        print(
+            f"{'| ID':<5}"
+            f"{'| Название':<30}"
+            f"{'| Автор':<25}"
+            f"{'| Издательство':<20}"
+            f"{'| Цена':<12}"
+            f"{'| Доступна':<10}|"
+        )
+        print("\n+" + "-" * 102 + "+")
+
+        for book in books:
+            print(
+                f"{book.id:<5}"
+                f"{book.title[:28]:<30}"
+                f"{book.author_name[:23]:<25}"
+                f"{book.publisher_name[:18]:<20}"
+                f"{str(book.price):<12}"
+                f"{'Да' if book.is_available else 'Нет':<10}"
+            )
+        print("\n+" + "-" * 102 + "+")
